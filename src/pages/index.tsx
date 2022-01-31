72ef988158698971704f458b8137e7fcbc80e21f
unknown <chromegold70@gmail.com>
2022-01-31 17:14:24 -0300

    Add styled-components and globalStyles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import type { NextPage } from 'next'
-
 export default function Home() {
   return <h1>Hello world</h1>
 }
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    min-height: 100%;
+    background: var(--gray-600);
+    color: var(--white)
+  }
+  
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  :root{
+    --white: #F8F8F2;
+    --pink: #FF79C6;
+    --gray-600: #383a59;
+    --gray-900: #282A36;
+  }
+`
