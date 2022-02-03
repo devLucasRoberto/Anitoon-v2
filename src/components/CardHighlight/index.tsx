cc8a675b665e8dd9e88159a028497e9411b376a2
unknown <chromegold70@gmail.com>
2022-02-03 02:38:41 -0300

    Add hover animations

--- src/components/CardHighlight/index.tsx
+++ src/components/CardHighlight/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Container, Img, Info, Title } from './styles'
+import { Container, Img, Info, Title, HoverInfo } from './styles'
 
 export function CardHighlight() {
   return (
@@ -9,6 +9,20 @@ export function CardHighlight() {
           <Img src="img/one-piece.png" alt="" />
           <Info>
             <Title>One Piece</Title>
+            <HoverInfo>
+              <p>
+                Gêneros: Ação, Aventura, Comédia, Drama, Fantasia, Shounen,
+                Super Poder
+              </p>
+              <p>Episódios: 1001 </p>
+              <p>Status Atual: Em Exibição </p>
+              <p>Autor: Eiichiro Oda </p>
+              <p>Estúdio: Toei Animation </p>
+              <p>Tipo: Dublado e Legendado </p>
+              <p>Temporada: Outono </p>
+              <p>Data de Exibição: 20 de out. de 1999 </p>
+              <p>Nota:  8.61 (891.6 mil votos)</p>
+            </HoverInfo>
           </Info>
         </a>
       </Link>
--- src/components/CardHighlight/styles.ts
+++ src/components/CardHighlight/styles.ts
@@ -1,26 +1,50 @@
 import styled from 'styled-components'
 
+export const Info = styled.div`
+  position: absolute;
+  text-align: center;
+  padding: 0.625rem 0.9375rem;
+  z-index: 1;
+  width: 100%;
+  max-height: 44px;
+  top: 0;
+  background: rgb(68, 71, 90, 0.7);
+  transition: max-height 0.3s;
+`
+export const HoverInfo = styled.div`
+  text-align: left;
+  color: var(--white);
+  font-size: 0.875rem;
+  font-weight: normal;
+  margin-top: 0.9375rem;
+  opacity: 0;
+  transition: opacity 0.4s;
+
+  p {
+    margin: 5px 0;
+  }
+`
 export const Container = styled.div`
   position: relative;
   max-width: 16.875rem;
   margin: 0 0.9375rem;
+
+  &:hover ${Info} {
+    max-height: 100%;
+    height: 100%;
+    align-items: initial;
+  }
+
+  &:hover ${HoverInfo} {
+    display: block;
+    opacity: 1;
+  }
 `
 export const Img = styled.img`
   max-width: 16.875rem;
   max-height: 25.375rem;
   height: 100%;
   width: 100%;
-`
-export const Info = styled.div`
-  position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 8px;
-  z-index: 1;
-  width: 100%;
-  top: 0;
-  background: rgb(68, 71, 90, 0.7);
 `
 export const Title = styled.h1`
   font-size: 1.25rem;
