dab93a0ded793c5f3141a379b899135507f27df9
unknown <chromegold70@gmail.com>
2022-02-06 01:24:00 -0300

    Add debounce

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,4 +1,6 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
+import { useDebounce, useDebouncedCallback } from 'use-debounce'
+import { api } from '../../services/api'
 import {
   Container,
   Input,
@@ -14,78 +16,60 @@ interface SearchProps {
 }
 
 export function Search({ show, setShow }: SearchProps) {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState<string>('')
+  const [searchResult, setSearchResult] = useState([])
+
+  const debounced = useDebouncedCallback(value => {
+    setValue(value)
+  }, 1000)
+
+  useEffect(() => {
+    if (value !== '') {
+      api
+        .get(`animes?q=${value}`)
+        .then(response => setSearchResult(response.data))
+    } else {
+      setSearchResult([])
+    }
+  }, [value])
+
+  console.log(value)
 
   function handleSearch(event: FormEvent) {
     event.preventDefault()
   }
-  console.log(value)
 
   return (
     <Container>
       <Input
         type="text"
         placeholder="Buscar Animes"
-        value={value}
+        defaultValue={value}
         onSubmit={handleSearch}
-        onChange={event => setValue(event.target.value)}
+        onChange={event => debounced(event.target.value)}
         onSelect={() => setShow(true)}
       />
 
       <ContainerSearch show={show ? 'block' : 'none'}>
-        <ContentSearch>
-          <Img src="img/one-piece.png" alt="one-piece" />
-          <Info>
-            <p>
-              Gêneros: Ação, Aventura, Comédia, Drama, Fantasia, Shounen, Super
-              Poder
-            </p>
-            <p>Episódios: 1001 </p>
-            <p>Status Atual: Em Exibição </p>
-            <p>Autor: Eiichiro Oda </p>
-            <p>Estúdio: Toei Animation </p>
-            <p>Tipo: Dublado e Legendado </p>
-            <p>Temporada: Outono </p>
-            <p>Data de Exibição: 20 de out. de 1999 </p>
-            <p>Nota:  8.61 (891.6 mil votos)</p>
-          </Info>
-        </ContentSearch>
-
-        <ContentSearch>
-          <Img src="img/one-piece.png" alt="one-piece" />
-          <Info>
-            <p>
-              Gêneros: Ação, Aventura, Comédia, Drama, Fantasia, Shounen, Super
-              Poder
-            </p>
-            <p>Episódios: 1001 </p>
-            <p>Status Atual: Em Exibição </p>
-            <p>Autor: Eiichiro Oda </p>
-            <p>Estúdio: Toei Animation </p>
-            <p>Tipo: Dublado e Legendado </p>
-            <p>Temporada: Outono </p>
-            <p>Data de Exibição: 20 de out. de 1999 </p>
-            <p>Nota:  8.61 (891.6 mil votos)</p>
-          </Info>
-        </ContentSearch>
-
-        <ContentSearch>
-          <Img src="img/one-piece.png" alt="one-piece" />
-          <Info>
-            <p>
-              Gêneros: Ação, Aventura, Comédia, Drama, Fantasia, Shounen, Super
-              Poder
-            </p>
-            <p>Episódios: 1001 </p>
-            <p>Status Atual: Em Exibição </p>
-            <p>Autor: Eiichiro Oda </p>
-            <p>Estúdio: Toei Animation </p>
-            <p>Tipo: Dublado e Legendado </p>
-            <p>Temporada: Outono </p>
-            <p>Data de Exibição: 20 de out. de 1999 </p>
-            <p>Nota:  8.61 (891.6 mil votos)</p>
-          </Info>
-        </ContentSearch>
+        {searchResult.map(animes => (
+          <ContentSearch key={animes.id}>
+            <Img src="img/one-piece.png" alt="one-piece" />
+            <Info>
+              <p>
+                Gêneros: Ação, Aventura, Comédia, Drama, Fantasia, Shounen,
+                Super Poder
+              </p>
+              <p>Episódios: 1001 </p>
+              <p>Status Atual: Em Exibição </p>
+              <p>Autor: Eiichiro Oda </p>
+              <p>Estúdio: Toei Animation </p>
+              <p>Tipo: Dublado e Legendado </p>
+              <p>Temporada: Outono </p>
+              <p>Data de Exibição: 20 de out. de 1999 </p>
+              <p>Nota:  8.61 (891.6 mil votos)</p>
+            </Info>
+          </ContentSearch>
+        ))}
       </ContainerSearch>
     </Container>
   )
